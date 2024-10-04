8d22fc8fbfc3ecfaac06f920f91e3ced7d04b744
dganesh22 <dganesh2022@gmail.com>
2024-10-04 15:15:58 +0530

    Crud api mern

---
+++ model/userModel.js
@@ -0,0 +1,37 @@
+// db schema
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    mobile: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    role: {
+        type: String,
+        default: "user"
+    },
+    address: {
+        type: String,
+        default: ""
+    }
+},{
+    collection: "users",
+    timestamps: true
+})
+
+module.exports = mongoose.model("User", userSchema)
